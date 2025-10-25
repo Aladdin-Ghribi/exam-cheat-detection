cbd7a7fa445b72e88b279bb29a1d1603a10810a2
Alan <alanghribideveloper@gmail.com>
2025-10-25 12:02:20 +0200

    feat: implement and tune YOLO detector for persons/objects - Week 2 Dev A

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ SAMPLES_DIR = os.path.join(DATA_DIR, "samples")
 OUTPUT_DIR = os.path.join(PROJECT_ROOT, "output")
 
 # Model
-YOLO_MODEL = "yolo11n.pt"
+YOLO_MODEL = "yolo11m.pt" # best spot atm
 
 # Inference
 CONFIDENCE_THRESHOLD = 0.5
-IMG_SIZE = 640
+IMG_SIZE = 832 # best spot atm
 
 # Ensure dirs exist
 os.makedirs(OUTPUT_DIR, exist_ok=True)
